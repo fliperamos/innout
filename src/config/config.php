e40b50543d6c3bf5773c43079b7b02a6903ae8bc
Felipe Ramos <framos14@gmail.com>
2021-06-28 23:10:35 -0300

    classe login

--- src/config/config.php
+++ src/config/config.php
@@ -4,10 +4,12 @@ setlocale(LC_TIME, "pt_BR", "pt_BR.utf-8", "portugueses");
 
 //PASTAS
 define("MODEL_PATH", realpath(dirname(__FILE__) . "/../models"));
+define("VIEW_PATH", realpath(dirname(__FILE__) . "/../views"));
 
 
 
 
-//DATABASE
+//ARQUIVOS
 require_once(realpath(dirname(__FILE__) . "/database.php"));
+require_once(realpath(MODEL_PATH . "/Model.php"));
 
